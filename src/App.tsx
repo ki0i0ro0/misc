70b6d6abd74172ce91a3ef8d1fe266630ab10acb
ki0i0ro0 <ki0i0ro0@gmail.com>
2022-06-10 20:14:02 +0900

    [add] login and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 // in src/App.js
 import * as React from 'react'
-import { Admin, Resource, EditGuesser } from 'react-admin'
+import { Admin, Resource } from 'react-admin'
 import { UserList } from './users'
 import jsonServerProvider from 'ra-data-json-server'
-import { PostList } from './posts'
+import { PostList, PostEdit, PostCreate } from './posts'
+import PostIcon from '@mui/icons-material/Book'
+import UserIcon from '@mui/icons-material/Group'
+import Dashboard from './Dashboard'
+import authProvider from './authProvider'
+
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={EditGuesser} />
-    <Resource name="users" list={UserList} />
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 )
 
