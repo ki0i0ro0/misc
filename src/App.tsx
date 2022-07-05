c00375bde4cc8b3bf6adae6da51382d247438bbb
ki0i0ro0 <23109879+ki0i0ro0@users.noreply.github.com>
2022-07-05 20:55:24 +0900

    [update] React関数化

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import React, { Component } from 'react'
+import { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 import Sam from './assets/sample.mp4'
 
-export default class Snow extends Component {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      PlayGrond: Sam,
-    }
-  }
+export default function Snow() {
+  const [url, setUrl] = useState('')
 
-  render() {
-    const { PlayGrond } = this.state as any
-    return (
-      <div>
-        <h1>動画再生アプリ</h1>
-        <ReactPlayer
-          url={PlayGrond}
-          id="MainPlay"
-          playing
-          loop
-          controls={true}
-          width="1280px"
-          height="720px"
-        />
-      </div>
-    )
-  }
+  useEffect(() => {
+    setUrl(Sam)
+  }, [])
+
+  return (
+    <div>
+      <h1>動画再生アプリ</h1>
+      <ReactPlayer
+        url={url}
+        id="MainPlay"
+        playing
+        loop
+        controls={true}
+        width="1280px"
+        height="720px"
+      />
+    </div>
+  )
 }
