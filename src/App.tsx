4a259a28dbc184275e654f3d523a8d9c318a9484
ki0i0ro0 <ki0i0ro0@gmail.com>
2022-08-22 17:18:12 +0900

    [update] home router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Apollo from './Apollo'
+import Axios from './Axios'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/axios'} element={<Axios />} />
+        <Route path={'/apollo'} element={<Apollo />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Home() {
+  return (
+    <div className="App">
+      <p>
+        <Link to="/axios">Axios</Link>
+      </p>
+      <p>
+        <Link to="/apollo">Apollo Client</Link>
+      </p>
+    </div>
+  )
 }
 
 export default App
