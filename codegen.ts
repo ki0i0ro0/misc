2bd3b2894cd975937213c7444f50aa9e4db4819d
ki0i0ro0 <ki0i0ro0@gmail.com>
2022-10-12 21:25:01 +0900

    [update] vue でcode生成をテスト

--- codegen.ts
+++ codegen.ts
@@ -3,15 +3,12 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:4000",
-  documents: "src/**/*.tsx",
+  schema: "schema.graphql",
+  documents: "src/**/*.vue",
   generates: {
     "src/gql": {
       preset: "client",
       plugins: []
-    },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
     }
   }
 };
