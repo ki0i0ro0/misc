66f4ae311b1ce949f90f7eef49505a5a00af1ed9
ki0i0ro0 <ki0i0ro0@gmail.com>
2022-11-02 22:44:55 +0900

    [add] cdkローカル実行環境

--- README.md
+++ README.md
@@ -6,9 +6,19 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+
+## 参考サイト
+
+https://dev.classmethod.jp/articles/cdk-local-develop/
+
+⇒ このままでは使えないので書き換えは必要
+
+SAM インストール
+
+https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/getting-started-install.html
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,42 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const env = process.env.ENV || 'local';
 
-    // The code that defines your stack goes here
+    // DynamoDBテーブル
+    const sampleTable = new dynamodb.Table(this, 'sampleTable', {
+      tableName: 'samples',
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda関数
+    const sampleLambda = new lambda.Function(this, 'sampleLambda', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('src/lambda/sample'),
+      timeout: cdk.Duration.seconds(60),
+      environment: {
+        ENV: env,
+      },
+    });
+    sampleTable.grantReadWriteData(sampleLambda);
+
+    // API Gateway
+    const sampleApi = new apigateway.RestApi(this, 'sampleApi', {
+      restApiName: 'sampleApi',
+    });
+
+    const samplesResource = sampleApi.root.addResource('samples');
+    const getSamplesIntegration = new apigateway.LambdaIntegration(
+      sampleLambda
+    );
+    samplesResource.addMethod('GET', getSamplesIntegration);
   }
 }
