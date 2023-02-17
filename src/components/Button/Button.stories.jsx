c203a635bcd0014db67a45ecb149a46578157620
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-02-17 22:05:41 +0900

    [update] ボタンサイズ追加

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,5 +1,4 @@
-import Button from "./Button";
-
+import Button from "../Button/Button";
 // export defaultでmetadataを設定後その下にストーリーを記述
 export default {
   title: "Common/Button",
@@ -7,5 +6,17 @@ export default {
 };
 
 export const HelloButton = () => <Button>Hello World!</Button>;
-export const Primary = () => <Button color="primary">Primary</Button>;
+export const Default = () => <Button>Default</Button>;
 export const Danger = () => <Button color="danger">Danger</Button>;
+export const Primary = () => <Button color="primary">Primary</Button>;
+export const PrimarySmall = () => (
+  <Button size="sm" color="primary">
+    Small
+  </Button>
+);
+
+export const PrimaryLarge = () => (
+  <Button size="lg" color="primary">
+    Large
+  </Button>
+);
