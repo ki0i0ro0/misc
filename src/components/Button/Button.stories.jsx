0aab54428bbf39e7c2881bfe7e630ca1881370d8
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-02-17 22:08:59 +0900

    [update] StorybookにArgsを適用

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -4,19 +4,35 @@ export default {
   title: "Common/Button",
   component: Button,
 };
+const Template = (args) => <Button {...args} />;
 
-export const HelloButton = () => <Button>Hello World!</Button>;
-export const Default = () => <Button>Default</Button>;
-export const Danger = () => <Button color="danger">Danger</Button>;
-export const Primary = () => <Button color="primary">Primary</Button>;
-export const PrimarySmall = () => (
-  <Button size="sm" color="primary">
-    Small
-  </Button>
-);
+export const Default = Template.bind({});
+Default.args = {
+  children: "Default",
+};
 
-export const PrimaryLarge = () => (
-  <Button size="lg" color="primary">
-    Large
-  </Button>
-);
+export const Danger = Template.bind({});
+Danger.args = {
+  children: "Danger",
+  color: "danger",
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Primary",
+  color: "primary",
+};
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  children: "Small",
+  color: "primary",
+  size: "sm",
+};
+
+export const PrimaryLarge = Template.bind({});
+PrimaryLarge.args = {
+  ...PrimarySmall.args,
+  children: "Large",
+  size: "lg",
+};
