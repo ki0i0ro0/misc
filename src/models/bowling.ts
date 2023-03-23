d00288f6e493d670d03f7c4ca7da7e103bd333e1
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-03-23 21:40:49 +0900

    [update] 一旦ストライクまでのケースを完了させる

--- src/models/bowling.ts
+++ src/models/bowling.ts
@@ -1,9 +1,24 @@
 export class Bowling {
   calc(value: string) {
     let sum = 0;
-    value.split("").forEach((value) => {
+    value.split("").forEach((value, index, array) => {
       if (!isNaN(value as any)) {
+        if (array.length - 1 == index) return;
         sum += Number(value);
+      }
+
+      if (value == "/") {
+        sum =
+          sum + -1 * Number(array[index - 1]) + 10 + Number(array[index + 1]);
+      }
+
+      if (value == "X") {
+        sum += 10;
+      }
+
+      if (value == "X" && index > 1) {
+        if (array.length - 1 == index) return;
+        sum += 20;
       }
     });
     return sum;
--- src/models/sample.test.ts
+++ src/models/sample.test.ts
@@ -7,4 +7,10 @@ describe("", () => {
   it("通常2", () => {
     expect(new Bowling().calc("9-9-9-9-9-9-9-9-9-8-")).toEqual(89);
   });
+  it("スペア", () => {
+    expect(new Bowling().calc("5/5/5/5/5/5/5/5/5/5/5")).toEqual(150);
+  });
+  it("ストライク", () => {
+    expect(new Bowling().calc("XXXXXXXXXXXX")).toEqual(300);
+  });
 });
