c23a701f3a3e3ca305e779ad43680064afb9a352
ki0i0ro0 <kensaku.matsumoto@x-factory.jp>
2023-03-24 16:08:18 +0900

    [update] おそらく完成

--- src/models/sample.test.ts
+++ src/models/sample.test.ts
@@ -7,10 +7,16 @@ describe("", () => {
   it("通常2", () => {
     expect(new Bowling().calc("9-9-9-9-9-9-9-9-9-8-")).toEqual(89);
   });
+  it("通常", () => {
+    expect(new Bowling().calc("9-9-9-9-9-9-9-9-9-91")).toEqual(91);
+  });
   it("スペア", () => {
     expect(new Bowling().calc("5/5/5/5/5/5/5/5/5/5/5")).toEqual(150);
   });
   it("ストライク", () => {
     expect(new Bowling().calc("XXXXXXXXXXXX")).toEqual(300);
+  });
+  it("ストライク多め", () => {
+    expect(new Bowling().calc("XXXXXXXXXXX9")).toEqual(299);
   });
 });
