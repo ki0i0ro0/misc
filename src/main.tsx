707ed071703c521a7a4f647fd052cb1cb7ecd19f
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-04-04 22:10:05 +0900

    Add web component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import * as ReactDOM from "react-dom/client";
+import reactToWebComponent from "react-to-webcomponent";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Greeting = ({ name }: { name: string }) => {
+  return <h1>Hello, {name}</h1>;
+};
+
+const WebGreeting = reactToWebComponent(Greeting, React, ReactDOM);
+
+customElements.define("web-greeting", WebGreeting);
