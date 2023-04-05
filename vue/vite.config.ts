016d3a5ae1d4016e48ffc5a20e3ef67d1cad41be
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-04-05 21:10:37 +0900

    Load web component made react in vue

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // ダッシュを含むすべてのタグをカスタム要素として扱う
+          isCustomElement: (tag) => tag.includes("-"),
+        },
+      },
+    }),
+  ],
+});
