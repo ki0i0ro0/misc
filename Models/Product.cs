cb7223209384f7e66f55cf63e329dd277605e9b0
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-04-30 22:39:34 +0900

    スキャフォールドの実行

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorAppTest.Models
+{
+    public partial class Product
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public int Price { get; set; }
+    }
+}
