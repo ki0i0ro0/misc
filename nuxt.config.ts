44e141a367de68896b491a27276fef679e4aade0
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-05-20 10:53:38 +0900

    Vuetify追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
+  build: {
+    transpile: ["vuetify"],
+  },
   modules: ["@pinia/nuxt"],
+  hooks: {
+    "vite:extendConfig": (config) => {
+      config.plugins!.push(vuetify());
+    },
+  },
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+  css: ["@/assets/main.scss"],
+  srcDir: "./src",
 });
