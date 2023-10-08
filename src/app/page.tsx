a42e1674a40f8cead792519f304ac86e29b17f69
ki0i0ro0 <ki0i0ro0@gmail.com>
2023-10-08 21:32:56 +0900

    App Router に対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,26 @@
 import Link from "next/link";
-import axios from "axios";
-import Image from "next/image";
-const axiosClient = axios.create({
-  baseURL: "https://ki0i0ro0.microcms.io/api/v1",
-  headers: {
-    "X-MICROCMS-API-KEY": "e882cb286ca1422c8ebdbd594b43d5fd3515",
-  },
-});
-interface Props {
-  blogs: any[];
-}
 
-export default function Home(props: Props) {
-  const { blogs } = props;
+export default async function Home() {
+  const data = await fetch(`https://ki0i0ro0.microcms.io/api/v1/pets`, {
+    headers: {
+      "X-MICROCMS-API-KEY": process.env.API_KEY ?? "",
+    },
+    cache: "force-cache",
+  });
+
+  const blogs: any[] = (await data.json()).contents;
+
   return (
     <div>
       <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
-              <a>{blog.message}</a>
+              <p>{blog.message}</p>
             </Link>
-            <img src={blog.image.url} alt={blog.id}></img>
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
-// データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async () => {
-  const data = await axiosClient.get("/pets");
-
-  return {
-    props: {
-      blogs: data.data.contents,
-    },
-  };
-};
