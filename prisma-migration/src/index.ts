b8e35ef569fb15f421062117b09e52d4a4acc6a9
ki0i0ro0 <kensaku.matsumoto@x-factory.jp>
2023-11-06 20:28:45 +0900

    prisma migration クライアントの追加

---
+++ prisma-migration/src/index.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {}
+
+async function get() {
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+  return allUsers;
+}
+
+async function create() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
+}
+
+async function update() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  });
+  console.log(post);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
