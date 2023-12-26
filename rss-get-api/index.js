3c8371e6f4c3962eb0adf9ae0f35b6697621ba26
ki0i0ro0 <kensaku.matsumoto@x-factory.jp>
2023-12-26 16:54:30 +0900

    rss-get-api にapi proxyを導入

--- rss-get-api/index.js
+++ rss-get-api/index.js
@@ -1,26 +1,72 @@
-let viewXML = (xmlDocument) => {
-  //取得した文字列をコンソール出力
-  console.log(xmlDocument);
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
 
-  //XML形式に変換
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(xmlDocument, "text/xml");
-  let rss = doc.documentElement.getElementsByTagName("item");
+http
+  .createServer(function (request, response) {
+    console.log("request ", request.url);
 
-  //HTMLタグの作成
-  for (let i = 0; i < rss.length; i++) {
-    //RSSから取得したタイトルとリンク情報を格納
-    let rssTitle = rss[i].getElementsByTagName("title")[0].textContent;
-    let rssLink = rss[i].getElementsByTagName("link")[0].textContent;
+    let filePath = "." + request.url;
+    if (filePath == "./rss") {
+      const URL = "https://ascii.jp/mac/rss.xml";
+      fetch(URL)
+        .then((response) => response.text())
+        .then((xmlData) => {
+          response.writeHead(200, { "Content-Type": "text/html" });
+          response.end(xmlData, "utf-8");
+        })
+        .catch((error) => {
+          console.log(error);
+          response.writeHead(500);
+          response.end("Sorry, check with the site admin for error: ");
+        });
+    } else {
+      if (filePath == "./") {
+        filePath = "./index.html";
+      }
 
-    //テンプレート文字列を使ってアンカータグを作成
-    const tagString = `<a href="${rssLink}">${rssTitle}</a><br/>`;
+      const extname = String(path.extname(filePath)).toLowerCase();
+      const mimeTypes = {
+        ".html": "text/html",
+        ".js": "text/javascript",
+        ".css": "text/css",
+        ".json": "application/json",
+        ".png": "image/png",
+        ".jpg": "image/jpg",
+        ".gif": "image/gif",
+        ".svg": "image/svg+xml",
+        ".wav": "audio/wav",
+        ".mp4": "video/mp4",
+        ".woff": "application/font-woff",
+        ".ttf": "application/font-ttf",
+        ".eot": "application/vnd.ms-fontobject",
+        ".otf": "application/font-otf",
+        ".wasm": "application/wasm",
+      };
 
-    //body以下にアンカータグを挿入
-    document.body.insertAdjacentHTML("beforeend", tagString);
-  }
-};
-const URL = "https://ascii.jp/mac/rss.xml";
-fetch(URL)
-  .then((response) => response.text())
-  .then((xmlData) => viewXML(xmlData));
+      const contentType = mimeTypes[extname] || "application/octet-stream";
+
+      fs.readFile(filePath, function (error, content) {
+        if (error) {
+          if (error.code == "ENOENT") {
+            fs.readFile("./404.html", function (error, content) {
+              response.writeHead(404, { "Content-Type": "text/html" });
+              response.end(content, "utf-8");
+            });
+          } else {
+            response.writeHead(500);
+            response.end(
+              "Sorry, check with the site admin for error: " +
+                error.code +
+                " ..\n"
+            );
+          }
+        } else {
+          response.writeHead(200, { "Content-Type": contentType });
+          response.end(content, "utf-8");
+        }
+      });
+    }
+  })
+  .listen(3000);
+console.log("Server running at http://127.0.0.1:3000/");
