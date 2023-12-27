0c3fe12b2aff79021631e4b7650120da1a459115
ki0i0ro0 <kensaku.matsumoto@x-factory.jp>
2023-12-27 20:40:06 +0900

    rss-get-api gaeへのデプロイ設定を追加

--- rss-get-api/server.js
+++ rss-get-api/server.js
@@ -1,28 +1,37 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const RSS_BASE_URL = "https://note.hr-forecaster.jp";
+const URLS = [
+  "/m/m9a73f77b56c7/rss",
+  "/m/m7c879fd89eb9/rss",
+  "/m/m24a606663ae4/rss",
+  "/m/m365f7b0172e9/rss",
+  "/m/m4e64e61ffcc5/rss",
+];
+const PORT = process.env.PORT || 3000;
 
 http
-  .createServer(function (request, response) {
+  .createServer(async function (request, response) {
     console.log("request ", request.url);
 
     let filePath = "." + request.url;
-    if (filePath == "./rss") {
-      const URL = "https://ascii.jp/mac/rss.xml";
-      fetch(URL)
-        .then((response) => response.text())
-        .then((xmlData) => {
-          response.writeHead(200, { "Content-Type": "text/html" });
-          response.end(xmlData, "utf-8");
-        })
-        .catch((error) => {
-          console.log(error);
-          response.writeHead(500);
-          response.end("Sorry, check with the site admin for error: ");
-        });
+    if (filePath == "./api/rss") {
+      const results = await Promise.all(
+        URLS.map((url) =>
+          fetch(RSS_BASE_URL + url).then((result) => result.text())
+        )
+      ).catch((error) => {
+        console.log(error);
+        response.writeHead(500);
+        response.end("Sorry, check with the site admin for error: ");
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(JSON.stringify(results), "utf-8");
     } else {
       if (filePath == "./") {
-        filePath = "./index.html";
+        filePath = "./public/index.html";
       }
 
       const extname = String(path.extname(filePath)).toLowerCase();
@@ -68,5 +77,8 @@ http
       });
     }
   })
-  .listen(3000);
-console.log("Server running at http://127.0.0.1:3000/");
+  .listen(PORT);
+
+if (process.env.NODE_ENV !== "production") {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
+}
