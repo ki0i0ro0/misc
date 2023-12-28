212b22ba297edde35561f8d6ed5ad153e275dc95
ki0i0ro0 <kensaku.matsumoto@x-factory.jp>
2023-12-28 14:40:06 +0900

    demoをコンポーネントとして切り出し

--- rss-get-api/server.js
+++ rss-get-api/server.js
@@ -1,6 +1,4 @@
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
 
 const RSS_BASE_URL = "https://note.hr-forecaster.jp";
 const URLS = [
@@ -13,68 +11,31 @@ const URLS = [
 const PORT = process.env.PORT || 3000;
 
 http
-  .createServer(async function (request, response) {
+  .createServer(async (request, response) => {
     console.log("request ", request.url);
 
     let filePath = "." + request.url;
     if (filePath == "./api/rss") {
-      const results = await Promise.all(
-        URLS.map((url) =>
-          fetch(RSS_BASE_URL + url).then((result) => result.text())
-        )
-      ).catch((error) => {
-        console.log(error);
+      try {
+        const results = await Promise.all(
+          URLS.map((url) =>
+            fetch(RSS_BASE_URL + url).then((result) => result.text())
+          )
+        );
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(results), "utf-8");
+      } catch (error) {
+        console.error(error);
         response.writeHead(500);
         response.end("Sorry, check with the site admin for error: ");
-      });
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end(JSON.stringify(results), "utf-8");
-    } else {
-      if (filePath == "./") {
-        filePath = "./public/index.html";
       }
-
-      const extname = String(path.extname(filePath)).toLowerCase();
-      const mimeTypes = {
-        ".html": "text/html",
-        ".js": "text/javascript",
-        ".css": "text/css",
-        ".json": "application/json",
-        ".png": "image/png",
-        ".jpg": "image/jpg",
-        ".gif": "image/gif",
-        ".svg": "image/svg+xml",
-        ".wav": "audio/wav",
-        ".mp4": "video/mp4",
-        ".woff": "application/font-woff",
-        ".ttf": "application/font-ttf",
-        ".eot": "application/vnd.ms-fontobject",
-        ".otf": "application/font-otf",
-        ".wasm": "application/wasm",
-      };
-
-      const contentType = mimeTypes[extname] || "application/octet-stream";
-
-      fs.readFile(filePath, function (error, content) {
-        if (error) {
-          if (error.code == "ENOENT") {
-            fs.readFile("./404.html", function (error, content) {
-              response.writeHead(404, { "Content-Type": "text/html" });
-              response.end(content, "utf-8");
-            });
-          } else {
-            response.writeHead(500);
-            response.end(
-              "Sorry, check with the site admin for error: " +
-                error.code +
-                " ..\n"
-            );
-          }
-        } else {
-          response.writeHead(200, { "Content-Type": contentType });
-          response.end(content, "utf-8");
-        }
-      });
+    } else if (process.env.NODE_ENV !== "production") {
+      // NOTE: ローカルデモ用機能
+      const { demo } = require("./demo");
+      demo(response, filePath);
+    } else {
+      response.writeHead(404, { "Content-Type": "application/json" });
+      response.end("404 - Not Found", "utf-8");
     }
   })
   .listen(PORT);
