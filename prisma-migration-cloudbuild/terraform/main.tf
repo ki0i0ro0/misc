6b34416cbfb0cdd8d0738d13d999b28b632f4772
ki0i0ro0 <23109879+ki0i0ro0@users.noreply.github.com>
2024-03-23 21:55:31 +0900

    Update main.tf: Add PostgreSQL instance and create user

--- prisma-migration-cloudbuild/terraform/main.tf
+++ prisma-migration-cloudbuild/terraform/main.tf
@@ -29,26 +29,28 @@ resource "google_project_service" "cloudrun_api" {
 }
 
 # Creates SQL instance (~15 minutes to fully spin up)
-resource "google_sql_database_instance" "default" {
-  name             = "mysql-instance-1"
-  region           = "us-central1"
-  database_version = "MYSQL_8_0"
-  root_password    = "abcABC123!"
-
+resource "google_sql_database_instance" "instance" {
+  name             = "postgres-instance"
+  region           = "asia-northeast1"
+  database_version = "POSTGRES_15"
   settings {
     tier = "db-f1-micro"
-    password_validation_policy {
-      min_length                  = 6
-      complexity                  = "COMPLEXITY_DEFAULT"
-      reuse_interval              = 2
-      disallow_username_substring = true
-      enable_password_policy      = true
-    }
   }
   # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
   # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
   deletion_protection = false
   depends_on          = [google_project_service.sqladmin_api]
+}
+
+resource "random_password" "pwd" {
+  length  = 16
+  special = false
+}
+
+resource "google_sql_user" "user" {
+  name     = "user"
+  instance = google_sql_database_instance.instance.name
+  password = random_password.pwd.result
 }
 
 # [START cloudrun_service_cloudsql_dbuser_secret]
